1fbea853e05765ca7b0543551aad355ff4967b46
vinkims <vincekigen@gmail.com>
2023-11-07 10:28:05 +0300

    Change to client-side rendering

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -2,22 +2,13 @@
 
 import React, { Fragment, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 
 import { CustomFilterProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const router = useRouter();
-
+const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
   const [ selected, setSelected ] = useState(options[0]);
-
-  const handleUpdateParams = (e: { title: string, value: string}) => {
-    const newPathname = updateSearchParams(title, e.value.toLowerCase());
-
-    router.push(newPathname);
-  }
 
   return (
     <div className="w-fit">
@@ -25,7 +16,7 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
         value={selected}
         onChange={(e) => {
           setSelected(e)
-          handleUpdateParams(e)
+          setFilter(e.value)
         }}
       >
         <div className="relative w-fit z-10">
