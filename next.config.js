d872b62f0bafef9433038237b487f5a7cccf86dd
vinkims <vincekigen@gmail.com>
2023-11-07 11:25:27 +0300

    add local config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['cdn.imagin.studio']
+    },
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    env: {
+        RAPID_API_KEY: process.env.RAPID_API_KEY
+    }
+}
 
 module.exports = nextConfig
